ee794ebb8c18a703fbce2a24dd1871068cc73a5a
ArijeetDiwan <71815213+ArijeetDiwan@users.noreply.github.com>
2020-11-26 23:17:04 +0530

    Add files via upload

---
+++ food.js
@@ -0,0 +1,39 @@
+class Food {
+
+    constructor() {
+         this.foodStock;
+         this.lastFed ;
+    }
+}
+
+getFoodStock(){
+  
+    }
+
+  
+
+  updateFoodStock(){
+  
+  }
+
+  deductFood(){
+
+
+  }
+
+  display(){
+milkImage=addImage(Milk(1).png);
+if(MilkBottle){
+
+
+
+}else{
+
+
+
+}
+
+
+  }
+
+
